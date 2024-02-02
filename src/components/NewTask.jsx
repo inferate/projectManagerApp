31fef9a7ca678ab4445b69fd2e75d2d1cfcd440b
Dimitar Atanasov <dimitar.atanasov@MAC122.local>
2024-02-02 18:56:14 +0200

    test commit

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react';
+import {useState} from 'react'
 
-function NewTask({ onAdd }) {
-  const [enteredTask, setEnteredTask] = useState('');
-  const isNotValidTask = enteredTask.trim() === '';
+function NewTask({onAdd}) {
+  const [enteredTask, setEnteredTask] = useState('')
+  const isNotValidTask = enteredTask.trim() === ''
   const handleChange = (e) => {
-    setEnteredTask(e.target.value);
-  };
+    setEnteredTask(e.target.value)
+  }
   function handleClick() {
     if (!isNotValidTask) {
-      onAdd(enteredTask);
-      setEnteredTask('');
+      onAdd(enteredTask)
+      setEnteredTask('')
     }
+    //test commit
   }
   return (
-    <div className="flex items-center gap-4">
+    <div className='flex items-center gap-4'>
       <input
         value={enteredTask}
-        type="text"
-        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        type='text'
+        className='w-64 px-2 py-1 rounded-sm bg-stone-200'
         onChange={handleChange}
       />
       <button
         onClick={handleClick}
-        className="text-stone-700 hover:text-stone-950"
+        className='text-stone-700 hover:text-stone-950'
       >
         Add Task
       </button>
     </div>
-  );
+  )
 }
 
-export default NewTask;
+export default NewTask
